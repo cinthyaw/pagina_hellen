21f9d350904c51cc35a3e44dd0da42fc05c1a415
Daniel Jimenez <danieljs_coro_cr@hotmail.com>
2015-09-20 23:42:34 -0700

    PHP

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <link href="http://s3.amazonaws.com/codecademy-content/courses/ltp/css/shift.css" rel="stylesheet">
+
+  <link rel="stylesheet" href="http://s3.amazonaws.com/codecademy-content/courses/ltp/css/bootstrap.css">
+  <link rel="stylesheet" href="main.css">
+<title>Salber | Publicidad y Promoci&oacute;n</title>
+</head>
+
+<body>
+  <div class="container">
+    <?php
+    include_once "includes/header.php";
+    ?>
+
+   <div class="row">
+     <div class="jumbotron">
+
+      <h1>Articulos Promocionales</h1>
+      <p>Encuentre todos los art&iacuteculos que necesita.</p>
+      <a href="#">Ver m&aacutes</a>
+
+    </div> 
+  </div>
+  <div class="row">
+    <div class="neighborhood-guides">
+
+      
+
+      <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+          <h2>Nuestros Productos</h2>
+          <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+          <div class="col-md-4">
+            <div class="thumbnail">
+              <img class="img_peq" src="http://www.frugalcouponliving.com/wp-content/uploads/2014/09/Screen-Shot-2014-09-08-at-10.57.42-AM.png" >
+            </div>
+            <div class="thumbnail">
+              <img class="img_peq" src="img/bolso.png" >
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="thumbnail">
+              <img class="img_peq" src="img/articulos.png" >
+            </div>
+            <div class="thumbnail">
+              <img  class="img_peq" src="img/vaso.png" >
+            </div>
+          </div>
+          <div class="col-md-4">
+           <div class="thumbnail">
+            <img class="img_magic" src="img/magic.png" >
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <?php
+  include_once "includes/footer.php";
+  ?>
+</div>
+
+
+
+
+</body>
+</html>
